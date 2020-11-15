3e70a27c6f52a1fca8ca068cada6488a3e13c039
Mat Sz <contact@matsz.dev>
2020-11-15 15:00:10 +0100

    removed unnecessary code

--- __tests__/ClientManager.test.ts
+++ __tests__/ClientManager.test.ts
@@ -3,8 +3,7 @@ import randomColor from 'randomcolor';
 
 import { ClientManager } from '../src/ClientManager';
 import { Client } from '../src/types/Client';
-import { MessageType, ActionMessageActionType } from '../src/types/MessageType';
-import { TargetedMessageModel, ActionMessageModel } from '../src/types/Models';
+import { MessageType } from '../src/types/MessageType';
 
 export class TestClient implements Client {
   readonly clientId = uuid();
@@ -49,54 +48,7 @@ describe('ClientManager', () => {
     expect(JSON.parse(client.lastMessage)).toMatchObject({
       type: MessageType.WELCOME,
       clientId: client.clientId,
-      clientColor: client.clientColor,
     });
-  });
-
-  it('keeps track of local clients', async () => {
-    const clientManager = new ClientManager();
-
-    const client1 = new TestClient();
-    client1.remoteAddress = '127.0.0.1';
-    client1.networkName = 'TEST';
-    clientManager.addClient(client1);
-
-    const client2 = new TestClient();
-    client2.remoteAddress = '127.0.0.2';
-    client2.networkName = 'TEST';
-    clientManager.addClient(client2);
-
-    const client3 = new TestClient();
-    client3.remoteAddress = '127.0.0.1';
-    client3.networkName = 'TEST';
-    clientManager.addClient(client3);
-
-    expect(clientManager.getLocalClients(client1)).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          clientId: client1.clientId,
-        }),
-        expect.objectContaining({
-          clientId: client3.clientId,
-        }),
-      ])
-    );
-    expect(clientManager.getLocalClients(client2)).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          clientId: client2.clientId,
-        }),
-      ])
-    );
-
-    clientManager.removeClient(client3);
-    expect(clientManager.getLocalClients(client1)).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          clientId: client1.clientId,
-        }),
-      ])
-    );
   });
 
   it('pings clients', async () => {
@@ -123,50 +75,6 @@ describe('ClientManager', () => {
 
     expect(JSON.parse(client3.lastMessage)).toMatchObject({
       type: MessageType.PING,
-    });
-  });
-
-  it('relays messages to target clients', async () => {
-    const clientManager = new ClientManager();
-
-    const client1 = new TestClient();
-    clientManager.addClient(client1);
-
-    const client2 = new TestClient();
-    clientManager.addClient(client2);
-
-    const targetedMessage: ActionMessageModel = {
-      type: MessageType.ACTION,
-      action: ActionMessageActionType.ACCEPT,
-      targetId: client2.clientId,
-      transferId: 'test',
-    };
-
-    clientManager.handleMessage(client1, targetedMessage);
-
-    expect(JSON.parse(client2.lastMessage)).toMatchObject({
-      type: MessageType.ACTION,
-    });
-  });
-
-  it('drops invalid messages', async () => {
-    const clientManager = new ClientManager();
-
-    const client1 = new TestClient();
-    clientManager.addClient(client1);
-
-    const client2 = new TestClient();
-    clientManager.addClient(client2);
-
-    const targetedMessage: TargetedMessageModel = {
-      type: MessageType.ACTION,
-      targetId: client2.clientId,
-    };
-
-    clientManager.handleMessage(client1, targetedMessage);
-
-    expect(JSON.parse(client2.lastMessage)).toMatchObject({
-      type: MessageType.WELCOME,
     });
   });
 });
--- src/types/MessageType.ts
+++ src/types/MessageType.ts
@@ -1,16 +1,4 @@
 export enum MessageType {
   WELCOME = 'welcome',
-  NAME = 'name',
-  TRANSFER = 'transfer',
-  ACTION = 'action',
-  NETWORK = 'network',
   PING = 'ping',
-  RTC_DESCRIPTION = 'rtcDescription',
-  RTC_CANDIDATE = 'rtcCandidate',
-}
-
-export enum ActionMessageActionType {
-  ACCEPT = 'accept',
-  REJECT = 'reject',
-  CANCEL = 'cancel',
 }
